228c54f9c9dff43b81b8f7e72a777b42c7094778
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 17:19:44 +0800

    Update 02_2_CombineGvcf_chrY.sh

--- 02/02_2_CombineGvcf_chrY.sh
+++ 02/02_2_CombineGvcf_chrY.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 
-software/gatk-4.0.11.0/gatk CombineGVCFs --java-options "-Xmx150g -Djava.io.tmpdir=temp/" -R ref/ucsc.no_hap.hg19.fa --intervals chrY:1-59373566 \
+software/gatk-4.3.0.0/gatk CombineGVCFs --java-options "-Xmx150g -Djava.io.tmpdir=temp/" -R ref/ucsc.no_hap.hg19.fa --intervals chrY:1-59373566 \
 --variant 02_gvcf/cohort.vcf.list -O 02_gvcf/cohort.chrY.g.vcf
