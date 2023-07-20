5e3df78e5dc2aed47de6b1162f827adafc86e2ad
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:43:06 +0800

    Update 03_1_Filter_chr2.sh

--- 03/03_1_Filter_chr2.sh
+++ 03/03_1_Filter_chr2.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 
-cd /share2/pub/lik/lik/Star_protocols
 software/vcftools/bin/vcftools --gzvcf 02_vcf/cohort.chr2.vcf.gz --max-missing 0.9 --recode --recode-INFO-all --out 03_filter/cohort.chr2.mis0.9
 
