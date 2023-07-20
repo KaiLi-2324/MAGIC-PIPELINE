bc811f064f70f65d179aa7b37fe2753ac1922ecb
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:47:39 +0800

    Update 03_8_SplitVcf_chr13.sh

--- 03/03_8_SplitVcf_chr13.sh
+++ 03/03_8_SplitVcf_chr13.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 
 
-cd /share/pub/likai/Star_protocols
 software/vcftools/bin/vcftools --vcf 03_combine/cohort_after_VQSR.vcf --chr chr13 --recode --recode-INFO-all --out 03_split/cohort_after_VQSR.chr13
