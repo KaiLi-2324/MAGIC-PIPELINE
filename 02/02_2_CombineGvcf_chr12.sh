8230b1e27be7aa6cb2aec9aebf0adad8cb902319
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:04:52 +0800

    Update 02_2_CombineGvcf_chr12.sh

--- 02/02_2_CombineGvcf_chr12.sh
+++ 02/02_2_CombineGvcf_chr12.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 
-cd /share2/pub/lik/lik/Star_protocols
 software/gatk-4.0.11.0/gatk CombineGVCFs --java-options "-Xmx150g -Djava.io.tmpdir=temp/" -R ref/ucsc.no_hap.hg19.fa --intervals chr12:1-133851895 \
 --variant 02_gvcf/cohort.vcf.list -O 02_gvcf/cohort.chr12.g.vcf
