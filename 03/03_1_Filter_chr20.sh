5023257ad25304d87736687f8f7236ef133da8ef
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:43:18 +0800

    Update 03_1_Filter_chr20.sh

--- 03/03_1_Filter_chr20.sh
+++ 03/03_1_Filter_chr20.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 
-cd /share2/pub/lik/lik/Star_protocols
 software/vcftools/bin/vcftools --gzvcf 02_vcf/cohort.chr20.vcf.gz --max-missing 0.9 --recode --recode-INFO-all --out 03_filter/cohort.chr20.mis0.9
 
