e2433c634377869220ea20a1941e22034307fb16
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:46:44 +0800

    Update 03_6_VQSRIndel.sh

--- 03/03_6_VQSRIndel.sh
+++ 03/03_6_VQSRIndel.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/bash
 
-
-#!/usr/bin/bash
-
-
 software/gatk-4.3.0.0/gatk --java-options "-Xmx248g" ApplyVQSR -R ref/ucsc.no_hap.hg19.fa -V 03_combine/cohort_VQSR.snp.recal.vcf --ts-filter-level 99.9 \
 --tranches-file 03_combine/cohort_VQSR.indel.tranches --recal-file 03_combine/cohort_VQSR.indel.recal -mode INDEL -O 03_combine/cohort_VQSR.snp.indel.recal.vcf
