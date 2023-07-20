730b714ba3d60cb64a12bb56e719b2171598712c
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:06:17 +0800

    Update 02_2_CombineGvcf_chr18.sh

--- 02/02_2_CombineGvcf_chr18.sh
+++ 02/02_2_CombineGvcf_chr18.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 
-cd /share2/pub/lik/lik/Star_protocols
 software/gatk-4.0.11.0/gatk CombineGVCFs --java-options "-Xmx150g -Djava.io.tmpdir=temp/" -R ref/ucsc.no_hap.hg19.fa --intervals chr18:1-78077248 \
 --variant 02_gvcf/cohort.vcf.list -O 02_gvcf/cohort.chr18.g.vcf
