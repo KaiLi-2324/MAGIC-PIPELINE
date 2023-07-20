2bea2850f205c0709f60c4c665c02fe630503103
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 17:25:31 +0800

    Update 02_3_GenotypeGvcf_chr21.sh

--- 02/02_3_GenotypeGvcf_chr21.sh
+++ 02/02_3_GenotypeGvcf_chr21.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 
-software/gatk-4.0.11.0/gatk GenotypeGVCFs --java-options "-Xmx150g -Djava.io.tmpdir=temp/" \
+software/gatk-4.3.0.0/gatk GenotypeGVCFs --java-options "-Xmx150g -Djava.io.tmpdir=temp/" \
 --variant 02_gvcf/cohort.chr21.g.vcf -new-qual true -R ref/ucsc.no_hap.hg19.fa -O 02_vcf/cohort.chr21.vcf.gz
 
