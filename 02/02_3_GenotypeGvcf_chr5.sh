c34e75159874e60449e6d877ca4f4ce53adca1f8
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:13:15 +0800

    Update 02_3_GenotypeGvcf_chr5.sh

--- 02/02_3_GenotypeGvcf_chr5.sh
+++ 02/02_3_GenotypeGvcf_chr5.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 
-cd /share2/pub/lik/lik/Star_protocols
 software/gatk-4.0.11.0/gatk GenotypeGVCFs --java-options "-Xmx150g -Djava.io.tmpdir=temp/" \
 --variant 02_gvcf/cohort.chr5.g.vcf -new-qual true -R ref/ucsc.no_hap.hg19.fa -O 02_vcf/cohort.chr5.vcf.gz
 
