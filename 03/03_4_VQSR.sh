3eafbdef150fcdd2c43e85b63a540c5f546cb49c
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:46:03 +0800

    Update 03_4_VQSR.sh

--- 03/03_4_VQSR.sh
+++ 03/03_4_VQSR.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 
-cd /share/pub/likai/Star_protocols/
 software/gatk-4.3.0.0/gatk --java-options "-Xmx248g" ApplyVQSR -R ref/ucsc.no_hap.hg19.fa -V 03_combine/cohort.miss0.1.vcf \
 --ts-filter-level 99.9 --tranches-file 03_combine/cohort_VQSR.snp.tranches --recal-file 03_combine/cohort_VQSR.snp.recal -mode SNP \
 -O 03_combine/cohort_VQSR.snp.recal.vcf
