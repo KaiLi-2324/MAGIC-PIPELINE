19342cb1b6eee16723a03d5f4ce94981887c5914
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:45:27 +0800

    Update 03_1_Filter_chrY.sh

--- 03/03_1_Filter_chrY.sh
+++ 03/03_1_Filter_chrY.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 
-cd /share2/pub/lik/lik/Star_protocols
 software/vcftools/bin/vcftools --gzvcf 02_vcf/cohort.chrY.vcf.gz --max-missing 0.9 --recode --recode-INFO-all --out 03_filter/cohort.chrY.mis0.9
 
