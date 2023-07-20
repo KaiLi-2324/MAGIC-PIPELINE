f28cb660d4657a17346ee7890038315bcf2abdb4
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-20 15:40:41 +0800

    Update 03_11_HardFiltering_chr9.sh

--- 03/03_11_HardFiltering_chr9.sh
+++ 03/03_11_HardFiltering_chr9.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 
-cd /share/pub/likai/Star_protocols
 software/vcftools/bin/vcftools --vcf 03_split/cohort_after_VQSR.chr9.AB.VF.vcf --max-alleles 2 --hwe 0.000001 --max-missing 0.9 \
 --recode --recode-INFO-all --out 03_split/cohort_after_VQSR.chr9.AB.GQ.DP.hwe.miss.vcf
 
