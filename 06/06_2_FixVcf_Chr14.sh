250f84d3a7c97610e22c780bbc418d11a74851b8
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-27 16:58:11 +0800

    Delete 06 directory

--- 06/06_2_FixVcf_Chr14.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-
-cd /share/pub/likai/Star_protocols
-source /share/pub/likai/Software/anaconda3/bin/activate /share/pub/likai/Software/anaconda3/envs/bio/
-vcffixup 06_variantannotation/cohort_combine_snpQC_sampleQC.chr14.recode.vcf > 06_variantannotation/cohort_combine_snpQC_sampleQC_fix.chr14.recode.vcf
-
--- 06/06_2_FixVcf_Chr5.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-
-cd /share/pub/likai/Star_protocols
-source /share/pub/likai/Software/anaconda3/bin/activate /share/pub/likai/Software/anaconda3/envs/bio/
-vcffixup 06_variantannotation/cohort_combine_snpQC_sampleQC.chr5.recode.vcf > 06_variantannotation/cohort_combine_snpQC_sampleQC_fix.chr5.recode.vcf
-
