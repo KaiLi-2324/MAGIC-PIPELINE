9d3c55e52556ad9707d42c3131a1f60857a6a579
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-07-27 16:56:46 +0800

    Delete 01 directory

--- 01/01_3_HaplotypeCaller.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-
-export _JAVA_OPTIONS=-Djava.io.tmpdir=temp/
-
-software/gatk-4.3.0.0/gatk --java-options "-Xmx8G" HaplotypeCaller -R ref/ucsc.no_hap.hg19.fa -I 01_realign/19BY0125.bam -L ref/Design_V2.hg19.bed -ERC BP_RESOLUTION \
--O 01_vcf/19BY0125_HaplotypeCaller.g.vcf && \
-software/bgzip 01_vcf/19BY0125_HaplotypeCaller.g.vcf && software/tabix -p vcf 01_vcf/19BY0125_HaplotypeCaller.g.vcf.gz
