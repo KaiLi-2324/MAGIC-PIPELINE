e61c7a1f3e3dd53668a8bef56a6a3a4506ec8752
KaiLi <50455057+lemonsky123@users.noreply.github.com>
2023-10-14 22:38:19 +0800

    Update AlleleBalanceBySample.R

--- scripts/AlleleBalanceBySample.R
+++ scripts/AlleleBalanceBySample.R
@@ -1,17 +1,17 @@
 # AB filter
-# 输入为vcf文件，按照format中的AD字段对应的位置，提取每个突变样本（0/1开始）的ad，计算ab = ad[2] / sum(ad)
-# 如果ab满足条件，不进行改变，输出，如果ab不满足条件，将0/1变为./.，输出
+# The input is a vcf file. According to the position corresponding to the AD field in the format, extract the ad of each mutation sample (starting from 0/1) and calculate ab = ad[2] / sum(ad)
+# If ab meets the condition, the output will not be changed. If ab does not meet the condition, 0/1 will be changed to ./. for output.
 
 library(stringr)
 library(magrittr)
 library(readr)
 library(getopt)
 
-# 参数描述
+# args
 spec <- matrix(
-  # 每行五个，第五个可选，也就是说第五列可以不写
-  # byrow 按行填充矩阵的元素
-  # ncol  每行填充五个元素
+  # There are five in each row, and the fifth one is optional, which means that the fifth column does not need to be written.
+  # byrow Fill the elements of a matrix row by row
+  # ncol  Fill each row with five elements
   c("input_vcf_file",  "v", 1, "character", "vcf file after vep",
     "output_file", "o", 1, "character",  "output_file_prefix",
     "minAB",  "i", 1, "double",  "min AB",
@@ -22,7 +22,7 @@ opt <- getopt(spec=spec)
 print(opt)
 if( !is.null(opt$help) || is.null(opt$input_vcf_file) || is.null(opt$output_file)  || 
     is.null(opt$minAB)){
-  # ... 这里你也可以自定义一些东放在里面
+  # ... Here you can also customize some things to put in it
   cat(paste(getopt(spec=spec, usage = T), "\n"))
   quit()
 }
@@ -37,8 +37,8 @@ ln_num <- 1
 ln=readLines(con,n=1)
 while(length(ln) != 0) {
   if(exists("vcf.col.nam")){
-    vcf.col <- ln %>% str_split(., pattern = "\t") %>% unlist # 得到vcf文件的列
-    format.col <- vcf.col[format.idx] %>% str_split(., pattern = ":") %>% unlist # 得到vcf文件中FORMAT字段内的列
+    vcf.col <- ln %>% str_split(., pattern = "\t") %>% unlist # Get the columns of vcf file
+    format.col <- vcf.col[format.idx] %>% str_split(., pattern = ":") %>% unlist # Get the columns in the FORMAT field in the vcf file
     ad.idx <- str_which(format.col, pattern = "AD")
     mtt.idx <- grep("^0/1", vcf.col)
     vcf.col[mtt.idx] <- 
@@ -49,7 +49,7 @@ while(length(ln) != 0) {
           str_split(., pattern = ",") %>% unlist %>%
           as.numeric()
         ab <- ad[2]/sum(ad)
-        if((ab < minAB) | is.na(ab) | (ab > maxAB) ){ # 如果AB不满足0.25的条件，替换为./. ；如果0/1:.:0,0会报错，极特殊情况
+        if((ab < minAB) | is.na(ab) | (ab > maxAB) ){ # If AB does not meet the condition of 0.25, replace it with ./.; if 0/1:.:0,0 will report an error, which is a very special case.
           str_sub(dat, 1, 3) <- "./."
         }
         return(dat)
@@ -65,7 +65,7 @@ while(length(ln) != 0) {
       format.idx <- which(vcf.col.nam=="FORMAT")
       sam.stt.idx <- format.idx + 1
       sam.end.idx <- length(vcf.col.nam)
-    } # 构建vcf列名
+    } # Build vcf column name
   }
   ln=readLines(con,n=1)
   ln_num <- ln_num+1
